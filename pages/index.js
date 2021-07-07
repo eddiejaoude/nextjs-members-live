8e2976f21c56b68272573afc2e67f8afc881f111
Eddie Jaoude <eddie@jaoudestudios.com>
2021-07-07 19:11:48 +0100

    feat: ui for list (#2)

--- pages/index.js
+++ pages/index.js
@@ -16,54 +16,98 @@ export default function Home() {
           </a>
         </h1>
 
-        <p className="mt-3 text-2xl">
-          Get started by editing{' '}
-          <code className="p-3 font-mono text-lg bg-gray-100 rounded-md">
-            pages/index.js
-          </code>
-        </p>
-
-        <div className="flex flex-wrap items-center justify-around max-w-4xl mt-6 sm:w-full">
-          <a
-            href="https://nextjs.org/docs"
-            className="p-6 mt-6 text-left border w-96 rounded-xl hover:text-blue-600 focus:text-blue-600"
-          >
-            <h3 className="text-2xl font-bold">Documentation &rarr;</h3>
-            <p className="mt-4 text-xl">
-              Find in-depth information about Next.js features and API.
-            </p>
-          </a>
-
-          <a
-            href="https://nextjs.org/learn"
-            className="p-6 mt-6 text-left border w-96 rounded-xl hover:text-blue-600 focus:text-blue-600"
-          >
-            <h3 className="text-2xl font-bold">Learn &rarr;</h3>
-            <p className="mt-4 text-xl">
-              Learn about Next.js in an interactive course with quizzes!
-            </p>
-          </a>
-
-          <a
-            href="https://github.com/vercel/next.js/tree/master/examples"
-            className="p-6 mt-6 text-left border w-96 rounded-xl hover:text-blue-600 focus:text-blue-600"
-          >
-            <h3 className="text-2xl font-bold">Examples &rarr;</h3>
-            <p className="mt-4 text-xl">
-              Discover and deploy boilerplate example Next.js projects.
-            </p>
-          </a>
-
-          <a
-            href="https://vercel.com/import?filter=next.js&utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            className="p-6 mt-6 text-left border w-96 rounded-xl hover:text-blue-600 focus:text-blue-600"
-          >
-            <h3 className="text-2xl font-bold">Deploy &rarr;</h3>
-            <p className="mt-4 text-xl">
-              Instantly deploy your Next.js site to a public URL with Vercel.
-            </p>
-          </a>
-        </div>
+        <section className="text-gray-600 body-font">
+          <div className="container px-5 py-24 mx-auto">
+            <div className="flex flex-col text-center w-full mb-20">
+              <h1 className="sm:text-3xl text-2xl font-medium title-font mb-4 text-gray-900">Our Team</h1>
+              <p className="lg:w-2/3 mx-auto leading-relaxed text-base">Whatever cardigan tote bag tumblr hexagon brooklyn asymmetrical gentrify, subway tile poke farm-to-table. Franzen you probably haven't heard of them.</p>
+            </div>
+            <div className="flex flex-wrap -m-2">
+              <div className="p-2 lg:w-1/3 md:w-1/2 w-full">
+                <div className="h-full flex items-center border-gray-200 border p-4 rounded-lg">
+                  <img alt="team" className="w-16 h-16 bg-gray-100 object-cover object-center flex-shrink-0 rounded-full mr-4" src="https://dummyimage.com/80x80" />
+                  <div className="flex-grow">
+                    <h2 className="text-gray-900 title-font font-medium">Holden Caulfield</h2>
+                    <p className="text-gray-500">UI Designer</p>
+                  </div>
+                </div>
+              </div>
+              <div className="p-2 lg:w-1/3 md:w-1/2 w-full">
+                <div className="h-full flex items-center border-gray-200 border p-4 rounded-lg">
+                  <img alt="team" className="w-16 h-16 bg-gray-100 object-cover object-center flex-shrink-0 rounded-full mr-4" src="https://dummyimage.com/84x84" />
+                  <div className="flex-grow">
+                    <h2 className="text-gray-900 title-font font-medium">Henry Letham</h2>
+                    <p className="text-gray-500">CTO</p>
+                  </div>
+                </div>
+              </div>
+              <div className="p-2 lg:w-1/3 md:w-1/2 w-full">
+                <div className="h-full flex items-center border-gray-200 border p-4 rounded-lg">
+                  <img alt="team" className="w-16 h-16 bg-gray-100 object-cover object-center flex-shrink-0 rounded-full mr-4" src="https://dummyimage.com/88x88" />
+                  <div className="flex-grow">
+                    <h2 className="text-gray-900 title-font font-medium">Oskar Blinde</h2>
+                    <p className="text-gray-500">Founder</p>
+                  </div>
+                </div>
+              </div>
+              <div className="p-2 lg:w-1/3 md:w-1/2 w-full">
+                <div className="h-full flex items-center border-gray-200 border p-4 rounded-lg">
+                  <img alt="team" className="w-16 h-16 bg-gray-100 object-cover object-center flex-shrink-0 rounded-full mr-4" src="https://dummyimage.com/90x90" />
+                  <div className="flex-grow">
+                    <h2 className="text-gray-900 title-font font-medium">John Doe</h2>
+                    <p className="text-gray-500">DevOps</p>
+                  </div>
+                </div>
+              </div>
+              <div className="p-2 lg:w-1/3 md:w-1/2 w-full">
+                <div className="h-full flex items-center border-gray-200 border p-4 rounded-lg">
+                  <img alt="team" className="w-16 h-16 bg-gray-100 object-cover object-center flex-shrink-0 rounded-full mr-4" src="https://dummyimage.com/94x94" />
+                  <div className="flex-grow">
+                    <h2 className="text-gray-900 title-font font-medium">Martin Eden</h2>
+                    <p className="text-gray-500">Software Engineer</p>
+                  </div>
+                </div>
+              </div>
+              <div className="p-2 lg:w-1/3 md:w-1/2 w-full">
+                <div className="h-full flex items-center border-gray-200 border p-4 rounded-lg">
+                  <img alt="team" className="w-16 h-16 bg-gray-100 object-cover object-center flex-shrink-0 rounded-full mr-4" src="https://dummyimage.com/98x98" />
+                  <div className="flex-grow">
+                    <h2 className="text-gray-900 title-font font-medium">Boris Kitua</h2>
+                    <p className="text-gray-500">UX Researcher</p>
+                  </div>
+                </div>
+              </div>
+              <div className="p-2 lg:w-1/3 md:w-1/2 w-full">
+                <div className="h-full flex items-center border-gray-200 border p-4 rounded-lg">
+                  <img alt="team" className="w-16 h-16 bg-gray-100 object-cover object-center flex-shrink-0 rounded-full mr-4" src="https://dummyimage.com/100x90" />
+                  <div className="flex-grow">
+                    <h2 className="text-gray-900 title-font font-medium">Atticus Finch</h2>
+                    <p className="text-gray-500">QA Engineer</p>
+                  </div>
+                </div>
+              </div>
+              <div className="p-2 lg:w-1/3 md:w-1/2 w-full">
+                <div className="h-full flex items-center border-gray-200 border p-4 rounded-lg">
+                  <img alt="team" className="w-16 h-16 bg-gray-100 object-cover object-center flex-shrink-0 rounded-full mr-4" src="https://dummyimage.com/104x94" />
+                  <div className="flex-grow">
+                    <h2 className="text-gray-900 title-font font-medium">Alper Kamu</h2>
+                    <p className="text-gray-500">System</p>
+                  </div>
+                </div>
+              </div>
+              <div className="p-2 lg:w-1/3 md:w-1/2 w-full">
+                <div className="h-full flex items-center border-gray-200 border p-4 rounded-lg">
+                  <img alt="team" className="w-16 h-16 bg-gray-100 object-cover object-center flex-shrink-0 rounded-full mr-4" src="https://dummyimage.com/108x98" />
+                  <div className="flex-grow">
+                    <h2 className="text-gray-900 title-font font-medium">Rodrigo Monchi</h2>
+                    <p className="text-gray-500">Product Manager</p>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </section>
+        
       </main>
 
       <footer className="flex items-center justify-center w-full h-24 border-t">
