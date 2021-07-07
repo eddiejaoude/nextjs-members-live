03502e3ec477c7775569969f7a2109a988574e10
Eddie Jaoude <eddie@jaoudestudios.com>
2021-07-07 19:27:59 +0100

    feat: member component (#6)

--- pages/index.js
+++ pages/index.js
@@ -1,4 +1,5 @@
-import Head from 'next/head'
+import Head from "next/head";
+import Member from "../components/member";
 
 export default function Home() {
   return (
@@ -10,7 +11,7 @@ export default function Home() {
 
       <main className="flex flex-col items-center justify-center w-full flex-1 px-20 text-center">
         <h1 className="text-6xl font-bold">
-          Welcome to{' '}
+          Welcome to{" "}
           <a className="text-blue-600" href="https://nextjs.org">
             EddieHub!
           </a>
@@ -19,21 +20,57 @@ export default function Home() {
         <section className="text-gray-600 body-font">
           <div className="container px-5 py-24 mx-auto flex flex-wrap items-center">
             <div className="bg-gray-100 rounded-lg p-8 flex flex-col md:ml-auto w-full mt-10 md:mt-0">
-              <h2 className="text-gray-900 text-lg font-medium title-font mb-5">Sign Up</h2>
+              <h2 className="text-gray-900 text-lg font-medium title-font mb-5">
+                Sign Up
+              </h2>
               <div className="relative mb-4">
-                <label htmlFor="full-name" className="leading-7 text-sm text-gray-600">Full Name</label>
-                <input type="text" id="full-name" name="full-name" className="w-full bg-white rounded border border-gray-300 focus:border-indigo-500 focus:ring-2 focus:ring-indigo-200 text-base outline-none text-gray-700 py-1 px-3 leading-8 transition-colors duration-200 ease-in-out" />
+                <label
+                  htmlFor="full-name"
+                  className="leading-7 text-sm text-gray-600"
+                >
+                  Full Name
+                </label>
+                <input
+                  type="text"
+                  id="full-name"
+                  name="full-name"
+                  className="w-full bg-white rounded border border-gray-300 focus:border-indigo-500 focus:ring-2 focus:ring-indigo-200 text-base outline-none text-gray-700 py-1 px-3 leading-8 transition-colors duration-200 ease-in-out"
+                />
               </div>
               <div className="relative mb-4">
-                <label htmlFor="username" className="leading-7 text-sm text-gray-600">GitHub Username</label>
-                <input type="text" id="username" name="username" className="w-full bg-white rounded border border-gray-300 focus:border-indigo-500 focus:ring-2 focus:ring-indigo-200 text-base outline-none text-gray-700 py-1 px-3 leading-8 transition-colors duration-200 ease-in-out" />
+                <label
+                  htmlFor="username"
+                  className="leading-7 text-sm text-gray-600"
+                >
+                  GitHub Username
+                </label>
+                <input
+                  type="text"
+                  id="username"
+                  name="username"
+                  className="w-full bg-white rounded border border-gray-300 focus:border-indigo-500 focus:ring-2 focus:ring-indigo-200 text-base outline-none text-gray-700 py-1 px-3 leading-8 transition-colors duration-200 ease-in-out"
+                />
               </div>
               <div className="relative mb-4">
-                <label htmlFor="location" className="leading-7 text-sm text-gray-600">Location</label>
-                <input type="text" id="location" name="location" className="w-full bg-white rounded border border-gray-300 focus:border-indigo-500 focus:ring-2 focus:ring-indigo-200 text-base outline-none text-gray-700 py-1 px-3 leading-8 transition-colors duration-200 ease-in-out" />
+                <label
+                  htmlFor="location"
+                  className="leading-7 text-sm text-gray-600"
+                >
+                  Location
+                </label>
+                <input
+                  type="text"
+                  id="location"
+                  name="location"
+                  className="w-full bg-white rounded border border-gray-300 focus:border-indigo-500 focus:ring-2 focus:ring-indigo-200 text-base outline-none text-gray-700 py-1 px-3 leading-8 transition-colors duration-200 ease-in-out"
+                />
               </div>
-              <button className="text-white bg-indigo-500 border-0 py-2 px-8 focus:outline-none hover:bg-indigo-600 rounded text-lg">Button</button>
-              <p className="text-xs text-gray-500 mt-3">Literally you probably haven't heard of them jean shorts.</p>
+              <button className="text-white bg-indigo-500 border-0 py-2 px-8 focus:outline-none hover:bg-indigo-600 rounded text-lg">
+                Button
+              </button>
+              <p className="text-xs text-gray-500 mt-3">
+                Literally you probably haven't heard of them jean shorts.
+              </p>
             </div>
           </div>
         </section>
@@ -41,95 +78,23 @@ export default function Home() {
         <section className="text-gray-600 body-font">
           <div className="container px-5 py-24 mx-auto">
             <div className="flex flex-col text-center w-full mb-20">
-              <h1 className="sm:text-3xl text-2xl font-medium title-font mb-4 text-gray-900">Our Members</h1>
-              <p className="lg:w-2/3 mx-auto leading-relaxed text-base">EddieHub Members.</p>
+              <h1 className="sm:text-3xl text-2xl font-medium title-font mb-4 text-gray-900">
+                Our Members
+              </h1>
+              <p className="lg:w-2/3 mx-auto leading-relaxed text-base">
+                EddieHub Members.
+              </p>
             </div>
             <div className="flex flex-wrap -m-2">
-              <div className="p-2 lg:w-1/3 md:w-1/2 w-full">
-                <div className="h-full flex items-center border-gray-200 border p-4 rounded-lg">
-                  <img alt="team" className="w-16 h-16 bg-gray-100 object-cover object-center flex-shrink-0 rounded-full mr-4" src="https://dummyimage.com/80x80" />
-                  <div className="flex-grow">
-                    <h2 className="text-gray-900 title-font font-medium">Holden Caulfield</h2>
-                    <p className="text-gray-500">UI Designer</p>
-                  </div>
-                </div>
-              </div>
-              <div className="p-2 lg:w-1/3 md:w-1/2 w-full">
-                <div className="h-full flex items-center border-gray-200 border p-4 rounded-lg">
-                  <img alt="team" className="w-16 h-16 bg-gray-100 object-cover object-center flex-shrink-0 rounded-full mr-4" src="https://dummyimage.com/84x84" />
-                  <div className="flex-grow">
-                    <h2 className="text-gray-900 title-font font-medium">Henry Letham</h2>
-                    <p className="text-gray-500">CTO</p>
-                  </div>
-                </div>
-              </div>
-              <div className="p-2 lg:w-1/3 md:w-1/2 w-full">
-                <div className="h-full flex items-center border-gray-200 border p-4 rounded-lg">
-                  <img alt="team" className="w-16 h-16 bg-gray-100 object-cover object-center flex-shrink-0 rounded-full mr-4" src="https://dummyimage.com/88x88" />
-                  <div className="flex-grow">
-                    <h2 className="text-gray-900 title-font font-medium">Oskar Blinde</h2>
-                    <p className="text-gray-500">Founder</p>
-                  </div>
-                </div>
-              </div>
-              <div className="p-2 lg:w-1/3 md:w-1/2 w-full">
-                <div className="h-full flex items-center border-gray-200 border p-4 rounded-lg">
-                  <img alt="team" className="w-16 h-16 bg-gray-100 object-cover object-center flex-shrink-0 rounded-full mr-4" src="https://dummyimage.com/90x90" />
-                  <div className="flex-grow">
-                    <h2 className="text-gray-900 title-font font-medium">John Doe</h2>
-                    <p className="text-gray-500">DevOps</p>
-                  </div>
-                </div>
-              </div>
-              <div className="p-2 lg:w-1/3 md:w-1/2 w-full">
-                <div className="h-full flex items-center border-gray-200 border p-4 rounded-lg">
-                  <img alt="team" className="w-16 h-16 bg-gray-100 object-cover object-center flex-shrink-0 rounded-full mr-4" src="https://dummyimage.com/94x94" />
-                  <div className="flex-grow">
-                    <h2 className="text-gray-900 title-font font-medium">Martin Eden</h2>
-                    <p className="text-gray-500">Software Engineer</p>
-                  </div>
-                </div>
-              </div>
-              <div className="p-2 lg:w-1/3 md:w-1/2 w-full">
-                <div className="h-full flex items-center border-gray-200 border p-4 rounded-lg">
-                  <img alt="team" className="w-16 h-16 bg-gray-100 object-cover object-center flex-shrink-0 rounded-full mr-4" src="https://dummyimage.com/98x98" />
-                  <div className="flex-grow">
-                    <h2 className="text-gray-900 title-font font-medium">Boris Kitua</h2>
-                    <p className="text-gray-500">UX Researcher</p>
-                  </div>
-                </div>
-              </div>
-              <div className="p-2 lg:w-1/3 md:w-1/2 w-full">
-                <div className="h-full flex items-center border-gray-200 border p-4 rounded-lg">
-                  <img alt="team" className="w-16 h-16 bg-gray-100 object-cover object-center flex-shrink-0 rounded-full mr-4" src="https://dummyimage.com/100x90" />
-                  <div className="flex-grow">
-                    <h2 className="text-gray-900 title-font font-medium">Atticus Finch</h2>
-                    <p className="text-gray-500">QA Engineer</p>
-                  </div>
-                </div>
-              </div>
-              <div className="p-2 lg:w-1/3 md:w-1/2 w-full">
-                <div className="h-full flex items-center border-gray-200 border p-4 rounded-lg">
-                  <img alt="team" className="w-16 h-16 bg-gray-100 object-cover object-center flex-shrink-0 rounded-full mr-4" src="https://dummyimage.com/104x94" />
-                  <div className="flex-grow">
-                    <h2 className="text-gray-900 title-font font-medium">Alper Kamu</h2>
-                    <p className="text-gray-500">System</p>
-                  </div>
-                </div>
-              </div>
-              <div className="p-2 lg:w-1/3 md:w-1/2 w-full">
-                <div className="h-full flex items-center border-gray-200 border p-4 rounded-lg">
-                  <img alt="team" className="w-16 h-16 bg-gray-100 object-cover object-center flex-shrink-0 rounded-full mr-4" src="https://dummyimage.com/108x98" />
-                  <div className="flex-grow">
-                    <h2 className="text-gray-900 title-font font-medium">Rodrigo Monchi</h2>
-                    <p className="text-gray-500">Product Manager</p>
-                  </div>
-                </div>
-              </div>
+              <Member />
+              <Member />
+              <Member />
+              <Member />
+              <Member />
+              <Member />
             </div>
           </div>
         </section>
-        
       </main>
 
       <footer className="flex items-center justify-center w-full h-24 border-t">
@@ -139,10 +104,10 @@ export default function Home() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Powered by{' '}
+          Powered by{" "}
           <img src="/vercel.svg" alt="Vercel Logo" className="h-4 ml-2" />
         </a>
       </footer>
     </div>
-  )
+  );
 }
