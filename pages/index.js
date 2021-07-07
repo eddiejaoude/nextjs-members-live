98053648139aba5b9b4817025b0c3e1f0afd20b9
Eddie Jaoude <eddie@jaoudestudios.com>
2021-07-07 19:21:24 +0100

    feat: form to add members (#4)

--- pages/index.js
+++ pages/index.js
@@ -17,10 +17,32 @@ export default function Home() {
         </h1>
 
         <section className="text-gray-600 body-font">
+          <div className="container px-5 py-24 mx-auto flex flex-wrap items-center">
+            <div className="bg-gray-100 rounded-lg p-8 flex flex-col md:ml-auto w-full mt-10 md:mt-0">
+              <h2 className="text-gray-900 text-lg font-medium title-font mb-5">Sign Up</h2>
+              <div className="relative mb-4">
+                <label htmlFor="full-name" className="leading-7 text-sm text-gray-600">Full Name</label>
+                <input type="text" id="full-name" name="full-name" className="w-full bg-white rounded border border-gray-300 focus:border-indigo-500 focus:ring-2 focus:ring-indigo-200 text-base outline-none text-gray-700 py-1 px-3 leading-8 transition-colors duration-200 ease-in-out" />
+              </div>
+              <div className="relative mb-4">
+                <label htmlFor="username" className="leading-7 text-sm text-gray-600">GitHub Username</label>
+                <input type="text" id="username" name="username" className="w-full bg-white rounded border border-gray-300 focus:border-indigo-500 focus:ring-2 focus:ring-indigo-200 text-base outline-none text-gray-700 py-1 px-3 leading-8 transition-colors duration-200 ease-in-out" />
+              </div>
+              <div className="relative mb-4">
+                <label htmlFor="location" className="leading-7 text-sm text-gray-600">Location</label>
+                <input type="text" id="location" name="location" className="w-full bg-white rounded border border-gray-300 focus:border-indigo-500 focus:ring-2 focus:ring-indigo-200 text-base outline-none text-gray-700 py-1 px-3 leading-8 transition-colors duration-200 ease-in-out" />
+              </div>
+              <button className="text-white bg-indigo-500 border-0 py-2 px-8 focus:outline-none hover:bg-indigo-600 rounded text-lg">Button</button>
+              <p className="text-xs text-gray-500 mt-3">Literally you probably haven't heard of them jean shorts.</p>
+            </div>
+          </div>
+        </section>
+
+        <section className="text-gray-600 body-font">
           <div className="container px-5 py-24 mx-auto">
             <div className="flex flex-col text-center w-full mb-20">
-              <h1 className="sm:text-3xl text-2xl font-medium title-font mb-4 text-gray-900">Our Team</h1>
-              <p className="lg:w-2/3 mx-auto leading-relaxed text-base">Whatever cardigan tote bag tumblr hexagon brooklyn asymmetrical gentrify, subway tile poke farm-to-table. Franzen you probably haven't heard of them.</p>
+              <h1 className="sm:text-3xl text-2xl font-medium title-font mb-4 text-gray-900">Our Members</h1>
+              <p className="lg:w-2/3 mx-auto leading-relaxed text-base">EddieHub Members.</p>
             </div>
             <div className="flex flex-wrap -m-2">
               <div className="p-2 lg:w-1/3 md:w-1/2 w-full">
