dd8473e1e1a6c7129260261931d33088b614ab39
Eddie Jaoude <eddie@jaoudestudios.com>
2021-07-07 21:57:43 +0100

    feat: datastax read/write (#3)(#5)

--- pages/api/hello.js
+++ pages/api/hello.js
@@ -1,13 +1,42 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
+const { createClient } = require("@astrajs/collections");
 
-export default function helloAPI(req, res) {
-  console.log("API CALLED");
-  res.status(200).json([
-    { id: "1", name: "John Doe", github: "john-doe", location: "London" },
-    { id: "2", name: "Jane Doe", github: "john-doe", location: "London" },
-    { id: "3", name: "Emma Doe", github: "john-doe", location: "London" },
-    { id: "4", name: "Bret Doe", github: "john-doe", location: "London" },
-    { id: "5", name: "Martha Doe", github: "john-doe", location: "London" },
-    { id: "6", name: "Bob Doe", github: "john-doe", location: "London" },
-  ]);
+export default async function helloAPI(req, res) {
+  const astraClient = await createClient({
+    astraDatabaseId: process.env.ASTRA_DATABASE_ID,
+    astraDatabaseRegion: process.env.ASTRA_DATABASE_REGION,
+    applicationToken: process.env.ASTRA_APPLICATION_TOKEN,
+  });
+
+  const membersCollection = astraClient
+    .namespace("NextJS")
+    .collection("members");
+
+  // check if request is a post
+  if (req.method === "POST") {
+    // get the data from the request
+    const { name, github, location } = req.body;
+    console.log(name, github, location);
+    await membersCollection.create({
+      name: name,
+      github: github,
+      location: location,
+    });
+
+    res.status(201).json({
+      message: "Successfully added a new member",
+    });
+
+    return;
+  }
+
+  const members = await membersCollection.find({});
+  res.status(200).json(
+    Object.keys(members).map((key) => {
+      return {
+        id: key,
+        ...members[key],
+      };
+    })
+  );
 }
--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,27 @@
 import Head from "next/head";
+import { useState } from "react";
 import MemberList from "../components/MemberList";
 
 export default function Home() {
+  const [name, useName] = useState("");
+  const [github, useGithub] = useState("");
+  const [location, useLocation] = useState("");
+
+  const signup = async (event) => {
+    event.preventDefault();
+    const res = await fetch("/api/hello", {
+      body: JSON.stringify({
+        name,
+        github,
+        location,
+      }),
+      headers: {
+        "Content-Type": "application/json",
+      },
+      method: "POST",
+    });
+  };
+
   return (
     <div className="flex flex-col items-center justify-center min-h-screen py-2">
       <Head>
@@ -23,54 +43,62 @@ export default function Home() {
               <h2 className="text-gray-900 text-lg font-medium title-font mb-5">
                 Sign Up
               </h2>
-              <div className="relative mb-4">
-                <label
-                  htmlFor="full-name"
-                  className="leading-7 text-sm text-gray-600"
+              <form onSubmit={signup}>
+                <div className="relative mb-4">
+                  <label
+                    htmlFor="full-name"
+                    className="leading-7 text-sm text-gray-600"
+                  >
+                    Full Name
+                  </label>
+                  <input
+                    type="text"
+                    id="full-name"
+                    name="full-name"
+                    className="w-full bg-white rounded border border-gray-300 focus:border-indigo-500 focus:ring-2 focus:ring-indigo-200 text-base outline-none text-gray-700 py-1 px-3 leading-8 transition-colors duration-200 ease-in-out"
+                    onChange={(event) => useName(event.target.value)}
+                  />
+                </div>
+                <div className="relative mb-4">
+                  <label
+                    htmlFor="username"
+                    className="leading-7 text-sm text-gray-600"
+                  >
+                    GitHub Username
+                  </label>
+                  <input
+                    type="text"
+                    id="username"
+                    name="username"
+                    className="w-full bg-white rounded border border-gray-300 focus:border-indigo-500 focus:ring-2 focus:ring-indigo-200 text-base outline-none text-gray-700 py-1 px-3 leading-8 transition-colors duration-200 ease-in-out"
+                    onChange={(event) => useGithub(event.target.value)}
+                  />
+                </div>
+                <div className="relative mb-4">
+                  <label
+                    htmlFor="location"
+                    className="leading-7 text-sm text-gray-600"
+                  >
+                    Location
+                  </label>
+                  <input
+                    type="text"
+                    id="location"
+                    name="location"
+                    className="w-full bg-white rounded border border-gray-300 focus:border-indigo-500 focus:ring-2 focus:ring-indigo-200 text-base outline-none text-gray-700 py-1 px-3 leading-8 transition-colors duration-200 ease-in-out"
+                    onChange={(event) => useLocation(event.target.value)}
+                  />
+                </div>
+                <button
+                  type="submit"
+                  className="text-white bg-indigo-500 border-0 py-2 px-8 focus:outline-none hover:bg-indigo-600 rounded text-lg"
                 >
-                  Full Name
-                </label>
-                <input
-                  type="text"
-                  id="full-name"
-                  name="full-name"
-                  className="w-full bg-white rounded border border-gray-300 focus:border-indigo-500 focus:ring-2 focus:ring-indigo-200 text-base outline-none text-gray-700 py-1 px-3 leading-8 transition-colors duration-200 ease-in-out"
-                />
-              </div>
-              <div className="relative mb-4">
-                <label
-                  htmlFor="username"
-                  className="leading-7 text-sm text-gray-600"
-                >
-                  GitHub Username
-                </label>
-                <input
-                  type="text"
-                  id="username"
-                  name="username"
-                  className="w-full bg-white rounded border border-gray-300 focus:border-indigo-500 focus:ring-2 focus:ring-indigo-200 text-base outline-none text-gray-700 py-1 px-3 leading-8 transition-colors duration-200 ease-in-out"
-                />
-              </div>
-              <div className="relative mb-4">
-                <label
-                  htmlFor="location"
-                  className="leading-7 text-sm text-gray-600"
-                >
-                  Location
-                </label>
-                <input
-                  type="text"
-                  id="location"
-                  name="location"
-                  className="w-full bg-white rounded border border-gray-300 focus:border-indigo-500 focus:ring-2 focus:ring-indigo-200 text-base outline-none text-gray-700 py-1 px-3 leading-8 transition-colors duration-200 ease-in-out"
-                />
-              </div>
-              <button className="text-white bg-indigo-500 border-0 py-2 px-8 focus:outline-none hover:bg-indigo-600 rounded text-lg">
-                Button
-              </button>
-              <p className="text-xs text-gray-500 mt-3">
-                Literally you probably haven't heard of them jean shorts.
-              </p>
+                  Button
+                </button>
+                <p className="text-xs text-gray-500 mt-3">
+                  Literally you probably haven't heard of them jean shorts.
+                </p>
+              </form>
             </div>
           </div>
         </section>
